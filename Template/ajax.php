a2985144e131e62a03f8d1ef4fbf11f7173b1bd0
fatma Gelil <gelilfatma79@gmail.com>
2023-07-04 01:42:41 +0300

    first project

---
+++ Template/ajax.php
@@ -0,0 +1,18 @@
+<?php
+
+// require MySQL Connection
+require ('../database/DBController.php');
+
+// require Product Class
+require ('../database/Product.php');
+
+// DBController object
+$db = new DBController();
+
+// Product object
+$product = new Product($db);
+
+if (isset($_POST['item_id'])){
+    $result = $product->getData($_POST['item_id']);
+    echo json_encode($result);
+}
---
+++ header.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobile Shop</title>
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Owl-carousel CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha256-UhQQ4fxEeABh4JrcmAJ1+16id/1dnlOEVCFOxDef9Lw=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha256-kksNxjDRxd/5+jGurZUJd1sdR2v+ClrCl3svESBaJqw=" crossorigin="anonymous" />
+
+    <!-- font awesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+
+    <!-- Custom CSS file -->
+    <link rel="stylesheet" href="style.css">
+    <?php
+    //require database connection
+    require ('database/DBController.php');
+    $db = new DBController();
+    $db->con=null;
+   $file_path = 'database/DBController.php'; // the file path to check
+   if (file_exists($file_path)) {
+        require($file_path); // if the file exists, include it
+        echo "yes";
+    } else {
+       echo "Error: Could not find file '$file_path'"; // if the file does not exist, display an error message
+   }
+
+//    var_dump($con);
+//    $con=mysqli_connect('localhost','root','','shop');
+//    if($con->connect_error){
+//        echo 'Fail';
+//    }else{
+//        echo 'done';
+//    }
+    $servername = "localhost";
+    $database = "shop";
+    $username = "root";
+    $password = "";
+    $charset = "utf8mb4";
+    //    phpinfo();
+    //    echo ini_get('extension_dir');
+//        try {
+//        $dsn = "mysql:host=$servername;dbname=$database;charset=$charset";
+//        $pdo= new PDO($dsn,$username,$password);
+//        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//
+//                echo 'rr';
+//
+//                return $pdo;
+//
+//                }
+//
+//                catch (PDOException $e)
+//
+//                {
+//                echo  $e->getMessage();
+//                }
+
+//    var_dump('rr');
+//    print_r(PDO::getAvailableDrivers());
+    ?>
+</head>
+<body>
+
+<!-- start #header -->
+<header id="header">
+    <div class="strip d-flex justify-content-between px-4 py-1 bg-light">
+        <p class="font-rale font-size-12 text-black-50 m-0">Jordan Calderon 430-985 Eleifend St. Duluth Washington 92611 (427) 930-5255</p>
+        <div class="font-rale font-size-14">
+            <a href="#" class="px-3 border-right border-left text-dark">Login</a>
+            <a href="#" class="px-3 border-right text-dark">Whishlist (0)</a>
+        </div>
+    </div>
+
+    <!-- Primary Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark color-second-bg">
+        <a class="navbar-brand" href="#">Mobile Shopee</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav m-auto font-rubik">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">On Sale</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Category</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products <i class="fas fa-chevron-down"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Category <i class="fas fa-chevron-down"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Coming Soon</a>
+                </li>
+            </ul>
+            <form action="#" class="font-size-14 font-rale">
+                <a href="#" class="py-2 rounded-pill color-primary-bg">
+                    <span class="font-size-16 px-2 text-white"><i class="fas fa-shopping-cart"></i></span>
+                    <span class="px-3 py-2 rounded-pill text-dark bg-light">0</span>
+                </a>
+            </form>
+        </div>
+    </nav>
+    <!-- !Primary Navigation -->
+
+</header>
+<!-- !start #header -->
+
+<!-- start #main-site -->
+<main id="main-site">
